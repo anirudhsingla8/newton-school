5511c3ae6b66f191d15d5ba8a182d8c618897625
Shantanu Dwivedi <shantanu.dwvd@gmail.com>
2020-03-26 00:54:15 +0530

    Varaible name changed

--- python/bigger is greater.py
+++ python/bigger is greater.py
@@ -1,32 +1,32 @@
-def left_greater(arr):
+def left_greater(array):
     stack = []
     left = []
-    for i in range(0, len(arr)):
-        while len(stack) != 0 and stack[-1][0] <= arr[i]:
+    for i in range(0, len(array)):
+        while len(stack) != 0 and stack[-1][0] <= array[i]:
             stack.pop()
         if len(stack) == 0:
             left.append(-1)
         else:
             left.append(stack[-1][1])
-        stack.append([arr[i], i + 1])
+        stack.append([array[i], i + 1])
     return left
 
 
-def right_greater(arr):
+def right_greater(array):
     stack = []
     right = []
-    n = len(arr) - 1
+    n = len(array) - 1
     for i in range(n, -1, -1):
-        while len(stack) != 0 and stack[-1][0] <= arr[i]:
+        while len(stack) != 0 and stack[-1][0] <= array[i]:
             stack.pop()
         if len(stack) == 0:
             right.append(-1)
         else:
             right.append(stack[-1][1])
-        stack.append([arr[i], i + 1])
+        stack.append([array[i], i + 1])
     return right[::-1]
 
 
-arr = [6, 4, 6, 8, 2]
-print(left_greater(arr))
-print(right_greater(arr))
+array = [6, 4, 6, 8, 2]
+print(left_greater(array))
+print(right_greater(array))
